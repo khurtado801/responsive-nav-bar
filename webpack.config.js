8385735a1d2d8ecb66629cda23b200a62a8b76bf
ken.hurtado <khurtado@protonmail.com>
2019-06-11 15:27:46 -0600

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: path.join(__dirname,'src','index.js'),
